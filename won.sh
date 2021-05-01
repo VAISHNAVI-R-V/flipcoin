75f12b1254b5771e9b6ec158e4729aed0ad9e11a
VAISHNAVI-R-V <vaishucv14@gmail.com>
2021-05-01 09:26:15 -0700

    added: number of times head and tail has won

---
+++ won.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+RANDOM=$$
+
+PIPS=2
+MAXTHROWS=10
+throw=0
+
+Head=0
+Tails=0
+print_result ()
+{
+echo
+echo "Head =   $Head"
+echo "Tails =   $Tails"
+
+echo
+}
+
+update_count()
+{
+case "$1" in
+  0) let "Head += 1";;
+  1) let "Tails += 1";;
+
+esac
+}
+
+echo
+
+while [ "$throw" -lt "$MAXTHROWS" ]
+do
+  let "die1 = RANDOM % $PIPS"
+  update_count $die1
+  let "throw += 1"
+done
+
+print_result
+exit 0
